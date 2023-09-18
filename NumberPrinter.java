8a30283c34c0eb423b475f5ca1b3d3ccb097676c
John Doe <johndoe@email.com>
2023-09-18 22:14:39 +0530

    Add main.java file

---
+++ NumberPrinter.java
@@ -0,0 +1,13 @@
+public class NumberPrinter implements Runnable {
+
+    private int numToPrint=1;
+    
+  
+
+    @Override
+    public void run() {
+      numToPrint++;
+      System.out.println(numToPrint + " printed by thread = " + Thread.currentThread().getName());
+    }
+    
+}
